57a203ecc6c90b03d10120a0af60ff00cbd0015c
KingVishal703 <vishal21wankhede@gmail.com>
2024-12-08 19:12:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001860196646 -1002304788278"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002304788278"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
